29fc966c045dad292b290f807042bf93eebad5a4
Sebastian Rojas Osinaga <84255151+SebastianRojas627@users.noreply.github.com>
2021-09-06 12:26:27 -0400

    Create prim.cpp

---
+++ Algoritmos/Teoria_de_Grafos/Prim/prim.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define V 5
+
+int minKey(int key[], bool mstSet[])
+{
+    int min = INT_MAX;
+    int min_index;
+
+    for (int v = 0; v < V; v++)
+    {
+        if (mstSet[v] == false && key[v] < min)
+        {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+void printMST(int parent[], int key[])
+{
+    cout << "Arista  Peso" << endl;
+    for (int i = 1; i < V; i++)
+        cout << parent[i] << " - " << i << "    " << key[i] << endl;
+}
+
+void primMST(int graph[V][V])
+{
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+
+    for (int i = 0; i < V; i++)
+    {
+        key[i] = INT_MAX;
+        mstSet[i] = false;
+    }
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int i = 0; i < V - 1; i++)
+    {
+        int u = minKey(key, mstSet);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++)
+        {
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+            {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, key);
+}
+
+int main()
+{
+return 0;
+}
